e2cf0ae374e25018738f90fbb75ca8c1668fe1a9
Anna <anna.belowska2@gmail.com>
2019-03-29 20:21:40 +0100

    Add training CNN

---
+++ src/cnnClassifier/trainer.py
@@ -0,0 +1,20 @@
+from src.cnnClassifier.CNNFactory import CNNFactory
+
+
+class CNNTrainer:
+    def __init__(self, training_set, test_set):
+        self.training_set = training_set
+        self.test_set = test_set
+
+    def train(self, classifier=CNNFactory.create()):
+        # training_set = DataGenerator.get_train_set()
+        # test_set = DataGenerator.get_test_set()
+
+        classifier.fit_generator(
+            self.training_set,
+            steps_per_epoch=1000,
+            epochs=10,
+            validation_data=self.test_set,
+            validation_steps=100)
+
+        return classifier

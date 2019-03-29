5533dae051c335214b90806494bca27bad400638
Anna <anna.belowska2@gmail.com>
2019-03-29 19:57:35 +0100

    Create class for CNN factory

--- convolutionalNN.py
+++ convolutionalNN.py
@@ -4,15 +4,22 @@ from keras.layers import MaxPooling2D
 from keras.layers import Flatten
 from keras.layers import Dense
 
-classifier = Sequential()
 
-classifier.add(Convolution2D(32, 3, 3, input_shape=(64, 64, 3), activation='relu'))
+class CNNFactory:
 
-classifier.add(MaxPooling2D(pool_size=(2, 2)))
+    @staticmethod
+    def create():
+        classifier = Sequential()
 
-classifier.add(Flatten())
+        classifier.add(Convolution2D(32, 3, 3, input_shape=(64, 64, 3), activation='relu'))
 
-classifier.add(Dense(output_dim=128, activation='relu'))
-classifier.add(Dense(output_dim=1, activation='sigmoid'))
+        classifier.add(MaxPooling2D(pool_size=(2, 2)))
 
-classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+        classifier.add(Flatten())
+
+        classifier.add(Dense(output_dim=128, activation='relu'))
+        classifier.add(Dense(output_dim=1, activation='sigmoid'))
+
+        classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+        return classifier
